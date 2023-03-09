9a9fa896da7f613669d9fc0d75c2310babb0f96b
Anitor <anitorabraham@gmail.com>
2023-03-09 10:54:16 +0100

    made some changes to the users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import * as dotenv from 'dotenv';
 dotenv.config()
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
